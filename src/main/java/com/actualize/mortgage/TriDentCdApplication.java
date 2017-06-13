efc81ac321cd38ac0a6b67d65a1b6e8a6f824815
Shravan Kumar Boragala <shravan.boragala@compugain.com>
2017-06-13 13:30:16 +0530

    added @SpringBootApplication annotation

--- src/main/java/com/actualize/mortgage/TriDentCdApplication.java
+++ src/main/java/com/actualize/mortgage/TriDentCdApplication.java
@@ -3,12 +3,13 @@ package com.actualize.mortgage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * This class initiates the service for generation of JSON Object from MISMO XML and vice versa for closing disclosure
  * @author sboragala
  *
  */
-
+@SpringBootApplication
 public class TriDentCdApplication{
 	
 	private static final Logger LOG = LogManager.getLogger(TriDentCdApplication.class);
