67faebf3ed71b34ed06cd9233abcd0cc7879879a
Shravan Kumar Boragala <shravan.boragala@compugain.com>
2017-07-21 17:50:10 +0530

    added missed LoanAmortizationPeriodType to amortization rule

--- src/main/java/com/actualize/mortgage/cd/convertors/ClosingDisclosureConverter.java
+++ src/main/java/com/actualize/mortgage/cd/convertors/ClosingDisclosureConverter.java
@@ -230,14 +230,14 @@ public class ClosingDisclosureConverter {
     private ClosingDisclosureDocumentDetails createClosingDisclosureDocumentDetails(Document document) 
     {
     	ClosingDisclosureDocumentDetails closingDisclosureDocumentDetails = new ClosingDisclosureDocumentDetails();
+    	String type = "ClosingDisclosure";
+		String formType = "";
+		
     	if(null != document.documentClassification)
     	{
-    		String type = "";
-    		String formType = "";
     		DocumentClassification docClassification = new DocumentClassification(Document.NS, (Element)document.getElementAddNS("DOCUMENT_CLASSIFICATION"));
     		if(docClassification.documentClasses.documentClass.documentTypeOtherDescription.contains(":"))
     		{
-    			type = "ClosingDisclosure";
     			formType = docClassification.documentClasses.documentClass.documentTypeOtherDescription.split(":")[1];
     		}
     		closingDisclosureDocumentDetails.setDocumentType(type);
@@ -245,6 +245,8 @@ public class ClosingDisclosureConverter {
     	}
     	Deal deal = new Deal(Deal.NS, (Element)document.getElementAddNS("DEAL_SETS/DEAL_SET/DEALS/DEAL"));
         EscrowDetail escrowdetail = new EscrowDetail((Element)deal.getElementAddNS("LOANS/LOAN/ESCROW/ESCROW_DETAIL"));
+        
+        
         
         if("Seller".equalsIgnoreCase(escrowdetail.other.escrowAggregateAccountingAdjustmentPaidByType) )
         	closingDisclosureDocumentDetails.setEscrowAggregateAccountingAdjustmentAmountOthersPaid(escrowdetail.escrowAggregateAccountingAdjustmentAmount);
